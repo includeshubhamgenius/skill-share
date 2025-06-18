dca9068fcf15f5870b65820a1bd66343296c92c1
includeshubhamgenius <109446081+includeshubhamgenius@users.noreply.github.com>
2025-06-18 10:59:26 +0530

    Initial commit: Vite + React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center bg-gradient-to-br from-purple-400 to-blue-500 text-white text-3xl font-bold">
+      React+Tailwind+vite
+    </div>
+  );
+}
